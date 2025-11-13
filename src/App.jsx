9e2a5381526685b420768314554db23195722f78
SuggulaDharanipriya <sugguladharanipriya27@gmail.com>
2025-11-13 15:36:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - S105</h1>
+      <h1>CI/CD and Cloud DevOps Course</h1>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+      <h1>Done with Git Pages & Git Actions</h1>
+    </div>
+  );
+}
+export default App;
